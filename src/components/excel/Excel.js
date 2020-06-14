d9e224745f1fc75a3959cb3153bca4c2fba870b2
Timur <teekirienkov@gmail.com>
2020-06-14 21:29:50 +0300

    add components and core

---
+++ src/components/excel/Excel.js
@@ -0,0 +1,8 @@
+// global component Excel
+
+export class Excel {
+  constructor(selector, options) {
+    this.$el = document.querySelector(selector);
+    this.components = options.components || [];
+  }
+}
